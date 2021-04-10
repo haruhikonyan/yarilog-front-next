5110002c59929386741c88dc35f043c744e67d68
大谷東彦 <haruhiko@weseek.co.jp>
2021-04-10 17:46:17 +0900

    - Add eslint - Fix lint

--- components/List.tsx
+++ components/List.tsx
@@ -1,19 +1,18 @@
-import * as React from 'react'
-import ListItem from './ListItem'
-import { User } from '../interfaces'
+import ListItem from './ListItem';
+import { User } from '../interfaces';
 
 type Props = {
   items: User[]
 }
 
-const List = ({ items }: Props) => (
+const List: React.FC<Props> = ({ items }: Props) => (
   <ul>
-    {items.map((item) => (
+    {items.map(item => (
       <li key={item.id}>
         <ListItem data={item} />
       </li>
     ))}
   </ul>
-)
+);
 
-export default List
+export default List;
--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,16 +1,19 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { sampleUserData } from '../../../utils/sample-data'
+import { NextApiRequest, NextApiResponse } from 'next';
+import { sampleUserData } from '../../../utils/sample-data';
 
+// たぶん使わないので lint disabled
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const handler = (_req: NextApiRequest, res: NextApiResponse) => {
   try {
     if (!Array.isArray(sampleUserData)) {
-      throw new Error('Cannot find user data')
+      throw new Error('Cannot find user data');
     }
 
-    res.status(200).json(sampleUserData)
-  } catch (err) {
-    res.status(500).json({ statusCode: 500, message: err.message })
+    res.status(200).json(sampleUserData);
   }
-}
+  catch (err) {
+    res.status(500).json({ statusCode: 500, message: err.message });
+  }
+};
 
-export default handler
+export default handler;

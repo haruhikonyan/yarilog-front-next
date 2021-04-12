e8f706b540844a70f405e8f218a1f16f1013fd60
大谷東彦 <haruhiko@weseek.co.jp>
2021-04-12 15:51:25 +0900

    - getInitialProps ダメだったパターン

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
 import '../styles/app.scss';
 import type { AppProps } from 'next/app';
+import useSWR from 'swr';
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+import axios from '../utils/axios';
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  // useSWR('insturments', axios, { initialData: pageProps.insturments });
+  // useSWR('playstyles', axios, { initialData: pageProps.playstyles });
+  // useSWR('playing-logs/count-all', axios, { initialData: pageProps.playingsLogCount });
+  // useSWR('tunes/all-has-playing-log-tunes-count', axios, { initialData: pageProps.allHasPlayingLogTunesCount });
+  return <Component {...pageProps} />;
+};
+
+MyApp.getInitialProps = async() => {
+  // SSR 時のみ実行
+  if (typeof window === 'undefined') {
+  // TODO: API 呼び出し共通化
+    const insturments: any[] = (await axios.get('instruments')).data;
+    const playstyles: any[] = (await axios.get('playstyles')).data;
+    const playingsLogCount: number = (await axios.get('playing-logs/count-all')).data;
+    const allHasPlayingLogTunesCount: number = (await axios.get('tunes/all-has-playing-log-tunes-count')).data;
+
+    return {
+      insturments, playstyles, playingsLogCount, allHasPlayingLogTunesCount,
+    };
+  }
+  return {};
+};
 
 export default MyApp;

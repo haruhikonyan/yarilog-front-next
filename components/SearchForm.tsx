3ca2de5ef5c37193379ddf525e602e20641ba746
fujisawa <takuya@weseek.co.jp>
2021-04-26 19:32:35 +0000

    topページの枠組み作成

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -10,11 +10,12 @@ const SearchForm: React.FC<Props> = ({ placeholder, instruments }: Props) => {
     <>
       <form>
         <div className="d-flex">
-          <input type="text" placeholder={placeholder} />
-          <select className="w-auto rounded-0">
+          <input className="yrl-left-border-round form-control" type="text" placeholder={placeholder} />
+          <select className="w-auto rounded-0 custom-select">
+            <option value="">全楽器</option>
             {instruments.map(instrument => <option value={instrument.id}>{instrument.shortName}</option>)}
           </select>
-          <button type="submit" className="btn text-nowrap btn-primary"></button>
+          <button type="submit" className="btn text-nowrap btn-primary yrl-right-border-round"></button>
         </div>
       </form>
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,20 +12,21 @@ import { Instrument } from '../interfaces/Instrument';
 
 type Props = {
   playingLogs: PlayingLog[],
-  topPageLinkedComporsers: Composer[],
+  topPageLinkedComposers: Composer[],
   topPageLinkedGenres: Genre[],
   instruments: Instrument[],
 }
 
 const IndexPage: React.FC<Props> = ({
   playingLogs,
-  topPageLinkedComporsers,
+  topPageLinkedComposers,
   topPageLinkedGenres,
   instruments,
 }: Props) => {
 
   return (
     <>
+      {/* <header></header> */}
       <div className="yrl-top-title jumbotron jumbotron-fluid text-white text-center border-0 px-2 py-3 mt-n3 mb-3">
         <div className="container">
           <h2 className="mt-0 p-3">音楽を奏でる<br className="d-sm-none" />すべてのひとへ</h2>
@@ -40,15 +41,64 @@ const IndexPage: React.FC<Props> = ({
       </div>
       <div className="container">
         <div className="text-center">
-          <SearchForm placeholder="演奏記録を探す(フリーワード)" instruments={instruments}></SearchForm>
+          <section className="mt-3 mb-2">
+            <SearchForm placeholder="演奏記録を探す(フリーワード)" instruments={instruments}></SearchForm>
+            <div className="text-end text-primary mt-1 mr-3">
+              <span role="button" tabIndex={0}>詳細検索</span>
+            </div>
+          </section>
+          <div className="card mb-2">
+            <div className="card-body px-2 px-sm-3">
+              <h4 className="card-title">演奏楽器から探す</h4>
+              <div className="d-flex flex-wrap text-start">
+                {instruments.map(instrument => <a href={`/instruments/${instrument.id}`} className="yrl-instrument-link">&gt;{instrument.name}</a>)}
+              </div>
+            </div>
+          </div>
+          {/* <ins 広告></ins> */}
+          <p className="mt-3 mb-2">最新の演奏記録</p>
+          {/* Carousel */}
+          <div className="card mb-2">
+            <div className="card-body">
+              <h4 className="card-title">作曲家から探す</h4>
+              <div className="row text-start">
+                {/* eslint-disable-next-line max-len */}
+                {topPageLinkedComposers.map(composer => <a className="col-6 col-sm-4 col-md-3 col-lg-2 yrl-conposer-link" href={`/composers/${composer.id}`}>&gt;{composer.displayName}</a>)}
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <ins 広告></ins> */}
+        <div className="card mb-2 text-center">
+          <div className="card-body">
+            <h4 className="card-title">みゅーぐ開発において</h4>
+            <pre className="yrl-pre-wrap yrl-info text-start mb-0">
+              日本国内で楽器を演奏する人は人口の10%、つまり1200万人以上いると言われています。
+              {/* eslint-disable-next-line max-len */}
+              しかし、たくさんの人が曲や演奏にかける思いを共有するサービスは未だ存在しません。「この曲の素晴らしいところはここだ」「この曲は実はこんなことを考えながら演奏していた」そんなレビューを記録に残すことで、書く側は自身の思い出整理に用いたり、あるいは読む側はこれからの選曲会議での参考にしたり、レビュアーをエキストラに誘ってみたり、使い方は無限だと思っています。
+              是非、『みゅーぐ』を自由に活用してください。
+              楽器と演奏を愛するみなさんの活動の一助になれば幸いです。
+
+              なお、みゅーぐの開発を手伝ってくださる方を募集しています。
+              興味のある方は
+              <a href="/inquiry?inquiryTypeId=7">問い合わせフォーム</a>
+              もしくは
+              <a href="https://twitter.com/musig_net">公式Twitter</a>
+              などでご連絡ください。
+            </pre>
+          </div>
+        </div>
+        <div className="card mb-2 text-center">
+          <div className="card-body">
+            <h4 className="card-title">ジャンルから探す</h4>
+            <div className="d-flex flex-wrap text-start">
+              {topPageLinkedGenres.map(genre => <a href={`/genres/${genre.id}`} className="yrl-genre-link">&gt;{genre.name}</a>)}
+            </div>
+          </div>
         </div>
       </div>
-      <h1>PlayingLogs</h1>
-      {JSON.stringify(playingLogs)}
-      <h1>TopPageLinkedComporsers</h1>
-      {JSON.stringify(topPageLinkedComporsers)}
-      <h1>TopPageLinkedGenres</h1>
-      {JSON.stringify(topPageLinkedGenres)}
+      {/* <ins 広告></ins> */}
+      {/* <hooter></hooter> */}
     </>
   );
 };
@@ -60,13 +110,13 @@ export const getServerSideProps: GetServerSideProps = async() => {
       limit: 6,
     },
   })).data;
-  const topPageLinkedComporsers: any[] = (await axios.instance.get('composers/top-page-linked')).data;
+  const topPageLinkedComposers: any[] = (await axios.instance.get('composers/top-page-linked')).data;
   const topPageLinkedGenres: any[] = (await axios.instance.get('genres/top-page-linked')).data;
   const instruments: Instrument[] = (await axios.instance.get('instruments')).data;
   return {
     props: {
       playingLogs,
-      topPageLinkedComporsers,
+      topPageLinkedComposers,
       topPageLinkedGenres,
       instruments,
     },
